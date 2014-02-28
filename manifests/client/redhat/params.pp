5c7aaff29cb7775fd1048a333a6f463f06250062
Paul Houghton <paul4hough@gmail.com>
2014-02-27 18:37:08 -0700

    Fedora 20 support.

--- manifests/client/redhat/params.pp
+++ manifests/client/redhat/params.pp
@@ -3,10 +3,14 @@
 
 class nfs::client::redhat::params {
 
-  if versioncmp($::operatingsystemrelease, "6.0") > 0 {
+  if $::operatingsystemrelease == '20' {
+    $osmajor = 20
+  } elsif versioncmp($::operatingsystemrelease, "6.0") > 0 {
     $osmajor = 6
   } elsif versioncmp($::operatingsystemrelease, "5.0") > 0 {
     $osmajor = 5
+  } else {
+    fail("unsupported operatingsystemrelease: '${::operatingsystemrelease}'")
   }
 }
 
